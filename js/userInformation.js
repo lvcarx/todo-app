b54f1179fc4b4cc8650d8daf905fe8a02068f319
Luca <luca.reichmann@reichmann-it.de>
2019-10-12 13:52:41 +0200

    added user information to app

--- js/userInformation.js
+++ js/userInformation.js
@@ -1,2 +1,24 @@
-var mydata = JSON.parse(user);
-alert(mydata[0].name);
+
+// Username
+
+const userName = JSON.parse(user);
+const userNameHTML = document.getElementById('nameField');
+
+const userNode = document.createTextNode(userName[0].name);
+userNameHTML.appendChild(userNode);
+
+// E-Mail
+
+const userMail = JSON.parse(user);
+const userMailHTML = document.getElementById('mailField');
+
+const mailNode = document.createTextNode(userMail[0].mail);
+userMailHTML.appendChild(mailNode);
+
+// profile picture
+
+const userPicture = JSON.parse(user);
+const userPictureHTML = document.getElementById('profilePicture');
+
+const pictureNode = document.createTextNode(userPicture[0].picture);
+userPictureHTML.setAttribute("src", pictureNode.textContent);
