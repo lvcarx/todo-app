b4cf16d898e6213b0e2d64349187e9ddedf3b59d
Luca <luca.reichmann@reichmann-it.de>
2019-10-26 12:31:38 +0200

    fixed mode changing bug

--- js/settings.js
+++ js/settings.js
@@ -1,29 +1,82 @@
-// DARK MODE
 
-let darkModeOn = false;
+const isDarkModeActivatedOnStart = localStorage.getItem('mode');
 
-function switchToDark() {
+if (isDarkModeActivatedOnStart === 'true') {
 
     const bodys = document.getElementsByTagName('body');
     
-    for (var i = 0; i < bodys.length; i++) {
+    for (let i = 0; i < bodys.length; i++) {
 
-        bodys[i].classList.toggle('dark');
-        test.setVisible(darkModeOn);
-        darkModeOn = !darkModeOn;
+        bodys[i].classList.add('dark');
+
+    }
+
+} else {
+
+    const bodys = document.getElementsByTagName('body');
+    
+    for (let i = 0; i < bodys.length; i++) {
+
+        bodys[i].classList.remove('dark');
 
     }
 
 }
 
+
+
+// DARK MODE
+
+// let darkModeOn = false;
+
+let darkModeOn;
+
+if (isDarkModeActivatedOnStart === 'true') {
+
+    darkModeOn = 1;
+
+} else {
+
+    darkModeOn = 2;
+
+}
+
+function switchToDark() {
+
+    const bodys = document.getElementsByTagName('body');
+    
+    for (let i = 0; i < bodys.length; i++) {
+
+        bodys[i].classList.toggle('dark');
+        
+        darkModeOn += 1;
+        
+        if (darkModeOn%2 == 0) {
+    
+            console.log ('test');
+    
+            let darkModeActivated = false;
+            localStorage.setItem('mode', darkModeActivated);
+    
+        } else {
+
+            let darkModeActivated = true;
+            localStorage.setItem('mode', darkModeActivated); 
+
+        }
+
+} 
+
+}
+
+
 const darkModeToggle = document.getElementById('darkModeToggle');
 darkModeToggle.addEventListener('click', switchToDark, false);
 
 
+
+
+
 // Write Dark Settings to local storage
 
-let isVisible = true;
-$("#myBtn").on('click', function(){
-    panorama.setVisible(isVisible);
-    isVisible = !isVisible;
-});
+
