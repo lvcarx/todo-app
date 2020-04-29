24cf65ca2d2673845e6de19681b633bdc98be4d0
Luca Reichmann <luca.reichmann@reichmann-it.de>
2020-04-29 21:59:49 +0200

    push build

---
+++ client/build/precache-manifest.b04b0ae9c105f44379da2a8569a3b771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd045c5768c687367a73822c49f7b1dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44d30ed51870c6e20477",
+    "url": "/static/css/main.c2d300a4.chunk.css"
+  },
+  {
+    "revision": "38a67fa74461e699e3af",
+    "url": "/static/js/2.af2deb2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af2deb2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d30ed51870c6e20477",
+    "url": "/static/js/main.1d768a34.chunk.js"
+  },
+  {
+    "revision": "2800fcddd66813d244bf",
+    "url": "/static/js/runtime-main.5db5293e.js"
+  }
+]);
