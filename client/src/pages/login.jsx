d88d4ed563e51d30240cb970ebd4e654e132ab08
Luca Reichmann <luca.reichmann@reichmann-it.de>
2020-05-20 17:14:05 +0200

    added flash messages for errors during login and registering

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -12,7 +12,8 @@ class LoginPage extends React.Component {
         this.state = {
             email: '',
             password: '',
-            loggedIn: false
+            loggedIn: false,
+            loginProblems: ''
         }
     }
 
@@ -36,7 +37,17 @@ class LoginPage extends React.Component {
         }
         axios.post(`${process.env.REACT_APP_TEST}/api/users/login`, user)
             .then((resp) => {
-                if (resp.data) {
+                if (resp.data == "email-not-registered") {
+                    console.log("not registered!");
+                    this.setState({
+                        loginProblems: resp.data
+                    });
+                } else if(resp.data == "password-incorrect") {
+                    console.log("wrong pw!");
+                    this.setState({
+                        loginProblems: resp.data
+                    });
+                } else {
                     localStorage.setItem("user-token", resp.data);
                     window.location.reload(false);
                 }
@@ -52,43 +63,57 @@ class LoginPage extends React.Component {
     }
 
     render() {
-        return (
-            <div className="login">
-                <img className="logo" src="/img/logo.svg"></img>
-                <div>
-                    <h1>Login</h1>
-                    <p>A ToDo Manager that helps scheduling your day!</p>
-                    <form>
-                        <div className="form-group">
-                            <label>Email</label>
-                            <input
-                                type="email"
-                                id="email"
-                                name="email"
-                                className="form-control"
-                                placeholder="Enter Email"
-                                value={this.state.email}
-                                onChange={this.onChangeEmail}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input
-                                type="password"
-                                id="password"
-                                name="password"
-                                className="form-control"
-                                placeholder="Enter Password"
-                                value={this.state.password}
-                                onChange={this.onChangePassword}
-                            />
-                        </div>
-                        <p onClick={this.tryLogin} className="btn btn-primary dark-btn btn-block">Login</p>
-                    </form>
+            return (
+                <div className="login">
+                    <img className="logo" src="/img/logo.svg"></img>
+                    <div>
+                        <h1>Login</h1>
+                        <p>A ToDo Manager that helps scheduling your day!</p>
+
+                        {this.state.loginProblems == "email-not-registered" &&
+                            <div className="flashMessage">
+                                This E-Mail is not registered or your password is incorrect!
+                            </div>
+                        }
+
+                        {this.state.loginProblems == "password-incorrect" &&
+                            <div className="flashMessage">
+                                This E-Mail is not registered or your password is incorrect!
+                            </div>
+                        }
+    
+                        <form>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    className="form-control"
+                                    placeholder="Enter Email"
+                                    value={this.state.email}
+                                    onChange={this.onChangeEmail}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    className="form-control"
+                                    placeholder="Enter Password"
+                                    value={this.state.password}
+                                    onChange={this.onChangePassword}
+                                />
+                            </div>
+                            <p onClick={this.tryLogin} className="btn btn-primary dark-btn btn-block">Login</p>
+                        </form>
+                    </div>
+                    <p>No account? <a href="/#/register">Register</a></p>
                 </div>
-                <p>No account? <a href="/#/register">Register</a></p>
-            </div>
-        )
+            )
+         
     }
 
 }
