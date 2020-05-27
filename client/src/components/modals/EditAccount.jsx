fa217396eafdb325f979c3fb68560c7c3b14a503
Luca Reichmann <luca.reichmann@reichmann-it.de>
2020-05-28 01:28:20 +0200

    improved account editing

--- client/src/components/modals/EditAccount.jsx
+++ client/src/components/modals/EditAccount.jsx
@@ -11,13 +11,16 @@ class EditAccount extends React.Component {
         this.changePassword = this.changePassword.bind(this)
         this.isUserAllowedToChangePassword = this.isUserAllowedToChangePassword.bind(this)
         this.onChangeCurrentPassword = this.onChangeCurrentPassword.bind(this)
-        
+        this.onChangeNewPassword = this.onChangeNewPassword.bind(this)
+        this.onChangeNewPassword2 = this.onChangeNewPassword2.bind(this)
+        this.onChangeNewEmail = this.onChangeNewEmail.bind(this)
         this.state = {
             emailOpen: false,
             passwordOpen: false,
             currentPassword: '',
             newMail: '',
             newPassword: '',
+            newPassword2: '',
             allowed: false
         }
     }
@@ -34,9 +37,27 @@ class EditAccount extends React.Component {
         })
     }
 
+    onChangeNewEmail(e) {
+        this.setState({
+            newMail: e.target.value
+        });
+    }
+
     onChangeCurrentPassword(e) {
         this.setState({
             currentPassword: e.target.value
+        });
+    }
+
+    onChangeNewPassword(e) {
+        this.setState({
+            newPassword: e.target.value
+        });
+    }
+
+    onChangeNewPassword2(e) {
+        this.setState({
+            newPassword2: e.target.value
         });
     }
 
@@ -59,7 +80,7 @@ class EditAccount extends React.Component {
         const token = localStorage.getItem('user-token')
         const sendToken = {
             token: token,
-            mail: this.state.newMail
+            email: this.state.newMail
         }  
         axios.post(`${process.env.REACT_APP_TEST}/api/users/update`, sendToken)
             .then((res) => {
@@ -69,9 +90,11 @@ class EditAccount extends React.Component {
 
     changePassword() {
         const token = localStorage.getItem('user-token')
+        console.log(this.state.newPassword)
         const sendToken = {
             token: token,
-            password: this.state.newPassword
+            password: this.state.newPassword,
+            password2: this.state.newPassword2
         }  
         axios.post(`${process.env.REACT_APP_TEST}/api/users/update`, sendToken)
             .then((res) => {
@@ -85,37 +108,49 @@ class EditAccount extends React.Component {
                 <a id="close" onClick={this.props.openEditAccount} className="close"><img src="/img/close.svg"></img></a>
                 <h2>Edit your Account</h2>
 
-                
-                <h3>Change E-Mail Address</h3>
-                <a onClick={this.openEmailDialog}>Edit</a>
-                {this.state.emailOpen == true &&
-                <div>
-                    <label>Change your E-Mail
-                    <input type="text" name="email" id="" placeholder="Type your new E-Mail here..."/></label>
-                    <a onClick={this.changeEmail}>Save</a>
-                </div>
-                }
-                
-                <h3>Change Password</h3>
-                <a onClick={this.openPasswordDialog}>Change</a>
-                {this.state.passwordOpen == true &&
-                    <div>
-                        <label>Current password
-                        <input type="text" name="password" id="" onChange={this.onChangeCurrentPassword} placeholder="What's your current password?"/></label>
-                        <a onClick={this.isUserAllowedToChangePassword}>Save</a>
+                <div className="settingSection">
+                    <div className="wrapper">
+                        <h3>Change E-Mail Address</h3>
+                        <a onClick={this.openEmailDialog}>Edit</a>
                     </div>
-                }
-                    {this.state.allowed == true &&
-                        <div>
-                            <label>New password
-                            <input type="text" name="password" id="" placeholder=""/></label>
-                            <a onClick={this.changePassword}>Save</a>
+                    {this.state.emailOpen == true &&
+                    <div className="settingContent">
+                        <label>Change your E-Mail
+                        <input type="mail" name="email" id="" onChange={this.onChangeNewEmail} placeholder="Type your new E-Mail here..."/></label>
+                        <a onClick={this.changeEmail} className="button">Save</a>
+                    </div>
+                    }
+                </div>
+                
+                <div className="settingSection">
+                    <div className="wrapper">
+                        <h3>Change Password</h3>
+                        <a onClick={this.openPasswordDialog}>Change</a>
+                    </div>
+
+                    {(this.state.passwordOpen == true && this.state.allowed == false) &&
+                        <div className="settingContent">
+                            <label>Current password
+                            <input type="password" name="password" id="" onChange={this.onChangeCurrentPassword} placeholder="What's your current password?"/></label>
+                            <a onClick={this.isUserAllowedToChangePassword} className="button">Continue</a>
                         </div>
-                }
+                    }
+                        {(this.state.allowed == true && this.state.passwordOpen == true) &&
+                            <div className="settingContent">
+                                <label>New password
+                                <input type="password" name="password" id="" onChange={this.onChangeNewPassword} placeholder="Enter your new password"/>
+                                <input type="password" name="password2" id="" onChange={this.onChangeNewPassword2} placeholder="Confirm your new password"/></label>
+                                <a onClick={this.changePassword} className="button">Save</a>
+                            </div>
+                    }
+                </div>
 
-                <h3>Delete your Account and all personal data</h3>
-                <a onClick={this.openPasswordDialog}>Delete</a>
-
+                <div className="settingSection">
+                    <div className="wrapper">
+                        <h3>Delete your Account and all personal data</h3>
+                        <a onClick={this.openPasswordDialog}>Delete</a>
+                    </div>
+                </div>
             </div>
         )
     }
--- server/routes/users.js
+++ server/routes/users.js
@@ -143,13 +143,37 @@ router.post('/currentUser', auth, (req, res) => {
 
 router.post('/update', auth, (req, res) => {
     const decoded = jwtDecode(req.body.token);
-    console.log(decoded._id);
-     const { email, password, name } = req.body;
-     User.findOneAndUpdate({ _id: decoded, email: email, password: password }, req.body, function (err, data) {
-        if (!err) {
-            res.end();
+    let { email, password, name, password2 } = req.body;
+    let errors = []
+
+    // check passwords match
+    if (email < 6) {
+        errors.push({ msg: 'E-Mail too short.' });
+    }  
+    if (password < 6) {
+        errors.push({ msg: 'Password too short.' });
+    }  
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match.' });
+    } 
+    if (errors.length == 0) {
+        if (req.body.password != null) {
+            const saltRounds = 10;
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hash = bcrypt.hashSync(req.body.password, salt);
+            req.body.password = hash
         }
-    });
+
+        User.findOneAndUpdate({ _id: decoded}, req.body, function (err, data) {
+            if (!err) {
+                return res.send("User updated!");
+            } else {
+                console.log(err);
+            }
+        });
+    } else {
+        res.send(errors);
+    }
 }) 
 
 router.post('/delete', auth, (req, res) => {
