0898153b87cb5c42212483712f79e81d9b94b3d5
Luca Reichmann <luca.reichmann@reichmann-it.de>
2020-05-30 16:04:52 +0200

    interface improvements

--- client/src/components/modals/UserModal.jsx
+++ client/src/components/modals/UserModal.jsx
@@ -7,7 +7,8 @@ class UserModal extends React.Component {
         super(props)
         this.openModal = this.openModal.bind(this)
         this.tryLogout = this.tryLogout.bind(this)
-        
+        this.closeModal = this.closeModal.bind(this)
+
         this.state = {
             accountModalOpen: false,
             editAccount: false,
@@ -39,11 +40,20 @@ class UserModal extends React.Component {
         window.location.reload(false);
     }
 
+    closeModal() {
+        this.setState({
+            accountModalOpen: false
+        })
+    }
+
     render() {
         return (
             <div className="accountModalWrapper">
                     <img onClick={this.openModal} class="accountIcon" src="img/account.svg"></img>
-                    <div class={this.state.accountModalOpen == true ? 'accountModal opened' : 'accountModal'}><h3>Hi, {this.state.currentEmail}!</h3>
+                    <div class={this.state.accountModalOpen == true ? 'accountModal modal open' : 'modal accountModal'}>
+                        <a id="close" onClick={this.closeModal} className="close"><img src="/img/close.svg"></img></a>
+                        <h3>Hi, {this.state.currentEmail}!</h3>
+
                        <li><a onClick={this.tryLogout} class="link">Logout</a></li>
                        <li><a onClick={this.props.openEditAccount} class="link">User Settings</a></li>
                     </div>
